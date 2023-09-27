1cc573bd0509e9f1f348fc6ab78422f90dfec2fe
Shahbaz <shahbaz.ramzan@codedistrict.com>
2023-09-27 18:11:42 +0500

    add the animal input field with select option render list through map

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
 
+const ANIMALS = ["birds", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
   return (
     <div className="search-params">
       <form>
@@ -15,6 +18,21 @@ const SearchParams = () => {
             value={location}
             placeholder="location"
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
         </label>
         <button type="submit">Submit</button>
       </form>
