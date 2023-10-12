0f5f62d191ccbcb5c9540143301b2f8c25a517a9
Shahbaz <shahbaz.ramzan@codedistrict.com>
2023-10-12 19:28:34 +0500

    latest code

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -9,20 +9,28 @@ class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
+  handleIndexClick = (event) => {
+    this.setState({
+      active: +event.target.dataset.index,
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
-
     return (
       <div className="carousel">
-        <img src={images[active]} alt="animal hero" />
+        <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
               src={photo}
-              className={index === active ? active : ""}
-              alt="animal thumbnails"
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+              onClick={this.handleIndexClick}
+              data-index={index}
             />
           ))}
         </div>
